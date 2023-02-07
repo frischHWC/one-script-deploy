dfa8bbc354537206bf763cbe570e26a8296d5b2c
frisch <frisch@cloudera.com>
2023-02-07 14:32:12 +0100

    fix: correct nodes

--- launch.sh
+++ launch.sh
@@ -37,7 +37,7 @@ export CLUSTER_NAME="rocky"
     --debug=true \
     \
     --node-ipa="rockyipa.base.local" \
-    --node-base="rocky1.base.local rocky2.base.local rocky3.base.local rocky4.base.local rocky5.base.local rocky6.base.local rocky7.base.local" \
+    --nodes-base="rocky1.base.local rocky2.base.local rocky3.base.local rocky4.base.local rocky5.base.local rocky6.base.local rocky7.base.local" \
     --nodes-kts="rocky8.base.local" \
     \
     --install-repo-url="https://github.com/frischHWC/cldr-playbook/archive/refs/heads/${ANSIBLE_REPO_BRANCH}.zip" \

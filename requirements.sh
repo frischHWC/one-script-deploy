5aa80e1ef2e5098e9b072ff6ab041725887e018f
RISCH François <francois2.risch-prestataire@laposte.fr>
2023-06-02 14:15:54 +0200

    feat(reqs): prepare requirements for all distribution

--- requirements.sh
+++ requirements.sh
@@ -1,32 +1,55 @@
 #!/bin/bash
 # TODO: Make it for Debian, Suse & Mac
 
-### WARNING: Only Working on RHEL machines actually ####
+machine_type = $1
+
+if [ -z $machine_type ]
+then
+    machine_type = "rhel"
+fi
+
+echo "Will run requirements for $machine_type"
 
 ### Requirements to run this script #####
 
 echo "Set script runnable"
 chmod +x *.sh
 
-echo "Install wget"
-yum -y install wget
+if [ "$machine_type" == "rhel" ]
+then
 
-echo "Install Git"
-yum -y install git
+    echo "Install wget"
+    yum -y install wget
 
-echo "Install Unzip"
-yum -y install unzip
+    echo "Install Git"
+    yum -y install git
 
-echo "Install network packages"
-yum -y install dnsutils
-yum -y install bind-utils
+    echo "Install Unzip"
+    yum -y install unzip
 
-echo "Install Python 3"
-yum -y install python3
+    echo "Install network packages"
+    yum -y install dnsutils
+    yum -y install bind-utils
 
-echo "Install ansible"
-yum -y install epel-release
-yum -y install ansible
+    echo "Install Python 3"
+    yum -y install python3
+
+    echo "Install ansible"
+    yum -y install epel-release
+    yum -y install ansible
+
+
+
+elif [ "$machine_type" == "debian" ]
+then
+
+elif [ "$machine_type" == "suse" ]
+then
+
+elif [ "$machine_type" == "mac" ]
+then
+
+fi
 
 echo "Generates a local key"
 mkdir -p ~/.ssh/
